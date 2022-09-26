11367297d5b6490238b0b2aa0f5e1b6fcb564529
Danilo Lima <110578447+DaniloLima0707@users.noreply.github.com>
2022-09-26 15:17:03 -0400

    Implementando Contato para salvar no banco de dados

--- app/src/main/java/com/example/lista_telefonica2022/BancoDados/ContatoDB.java
+++ app/src/main/java/com/example/lista_telefonica2022/BancoDados/ContatoDB.java
@@ -1,4 +1,61 @@
 package com.example.lista_telefonica2022.BancoDados;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.lista_telefonica2022.Contato;
+
+import java.util.List;
+
 public class ContatoDB {
+    private DBHelper db;
+    private SQLiteDatabase conexao;
+
+    public ContatoDB(DBHelper db) { this.db = db; }
+
+    public void inserir(Contato contato) {
+        conexao = db.getWritableDatabase();//abre o bd
+        ContentValues valores = new ContentValues();
+        valores.put("campoNom", contato.getCampoNom());
+        valores.put("campoTel", contato.getCampoTel());
+        valores.put("campoDataNasc", contato.getCampoDataNasc());
+        conexao.update("Lista", valores, "id=?", new String[]{contato.getId().toString()});
+        conexao.close();
+    }
+
+    public void atualizar(Contato contato) {
+        conexao = db.getWritableDatabase();
+        ContentValues valores = new ContentValues();
+        valores.put("campoNom", contato.getCampoNom());
+        valores.put("campoTel", contato.getCampoTel());
+        valores.put("campoDataNasc", contato.getCampoDataNasc());
+        conexao.update("Lista", valores, "id=?", new String[]{contato.getId().toString()});
+        conexao.close();
+    }
+
+    public void remover(int id){
+        conexao = db.getWritableDatabase();
+        conexao.delete("Lista","id=?", new String[]{id + ""});
+    }
+
+    public void lista(List dados){
+        dados.clear();
+        conexao = db.getReadableDatabase();
+        String names[] = {"id", "campoNome", "campoTelefone", "campoDataNascimento"};
+        Cursor query = conexao.query("Lista", names, null, null, null, null, "campoNome");
+        while (query.moveToNext()){
+            Contato contato = new Contato();
+            contato.setId(Integer.parseInt(
+                    query.getString(0)));
+            contato.setCampoNom(
+                    query.getString(1));
+            contato.setCampoTel(
+                    query.getString(2));
+            contato.setCampoDataNasc(
+                    query.getString(3));
+            dados.add(contato);
+        }
+        conexao.close();
+    }
 }
--- app/src/main/java/com/example/lista_telefonica2022/Contato.java
+++ app/src/main/java/com/example/lista_telefonica2022/Contato.java
@@ -1,5 +1,32 @@
 package com.example.lista_telefonica2022;
 
 public class Contato {
+    private String campoNom;
+    private String campoTel;
+    private String campoDataNasc;
+    private Integer id;
 
+    public String getCampoNom() {return campoNom; }
+
+    public void setCampoNom(String campoNom) { this.campoNom = campoNom; }
+
+    public String getCampoTel() {return campoTel; }
+
+    public void setCampoTel(String campoTel) { this.campoTel = campoTel; }
+
+    public String getCampoDataNasc() {return campoDataNasc; }
+
+    public void setCampoDataNasc(String campoDataNasc) { this.campoDataNasc = campoDataNasc; }
+
+    public Integer getId() { return id; }
+
+    public void setId(Integer id) { this.id = id; }
+
+    @Override
+    public String toString() {
+        return
+                "Nome - " + campoNom +
+                        " | Tel - " + campoTel +
+                        " | Nasc - " + campoDataNasc;
+    }
 }
